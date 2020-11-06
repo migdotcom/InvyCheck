e209adce70a162436474fefda0aa5c0758cc8ec1
Loi Le <leducloi11@gmail.com>
2020-11-05 22:44:42 -0600

    push first material-ui

--- react/src/App.js
+++ react/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Container from '@material-ui/core/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.routes = [
+      {path: '/', Component: Home}
+    ];
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Container className="container">
+              {this.routes.map(({path, Component}) => (
+                <Route key={path} exact path={path}>
+                  {({match}) => (
+                      <div className="page">
+                        <Component/>
+                      </div>
+                  )}
+                </Route>
+              ))}
+            </Container>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
